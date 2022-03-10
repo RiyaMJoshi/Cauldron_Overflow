f5f762bf52dfc529c4175bc3fef72059bb61d31d
Riya Joshi <riyajoshi312@gmail.com>
2022-03-10 15:45:07 +0530

    Symfony 5 Fundamentals: Services, Config & Environments - Upto Chapter17

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,51 +1,65 @@
 <?php
-
 namespace App\Controller;
 
-use Twig\Environment;
+use Psr\Log\LoggerInterface;
 use App\Service\MarkdownHelper;
+use Twig\Environment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController extends AbstractController{
+class QuestionController extends AbstractController
+{
+    private $logger;
+    private $isDebug;
 
     /**
-     * @Route("/", name="app_homepage")
+     * @Route("/",name="app_homepage")
      */
+    public function __construct(LoggerInterface $logger, bool $isDebug){
+        $this->logger = $logger;
+        $this->isDebug = $isDebug;
+    }
+
+
     public function homepage(Environment $twigEnvironment)
-    {   
-        /*
-            // fun example of using twig services directly
-            $html = $twigEnvironment->render('question/homepage.html.twig');
-            return new Response($html);
-        */
+    {
+        // $html = $twigEnvironment->render('question/homepage.html.twig');
         return $this->render('question/homepage.html.twig');
+        // return new Response($html);
     }
-
-    /**
-     * @Route("questions/{slug}", name="app_question_show")
+     /**
+     * @Route("/questions/{slug}",name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper){
-        $answers = [
-            'Make sure your cat is sitting `purrrfectly` still 🤣',
-            'Honestly, I like furry shoes better than MY cat',
-            'Maybe... try saying the spell backwards?',
-        ];
+    public function show($slug,MarkdownHelper $markdownHelper)
+    {
+        if ($this->isDebug) {
+            $this->logger->info('We are in Debug Mode!');
+        }
+        // dump($this->getParameter('cache_adapter'));
+        $answers=['make sure your cat is sitting `perfectly`',
+                'furry shoes better than cat',
+                'try it backwards'    
+    ];
+    $questionText="I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.";
+ 
+    $parsedQuestionText = $markdownHelper->parse($questionText);
 
-        $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-        $parsedQuestionText = $parsedQuestionText = $markdownHelper->parse($questionText);
-        
-        // dump($cache);
-        // dump($this);
-        return $this->render('question/show.html.twig', [
-            'question' => ucwords(str_replace('-',' ',$slug)),
-            'questionText' => $parsedQuestionText,
-            'answers' => $answers
+   // dump($slug,$this);
+   // dd($markdownParser);
+  // dump($cache);
+      
+        return $this->render('question/show.html.twig',[
+            'question'=> ucwords(str_replace('-', ' ', $slug)),
+            'questionText'=>$parsedQuestionText,
+            'answers'=>$answers,
         ]);
-        // return new Response(sprintf('Future page to show question "%s"!',
-        // ucwords(str_replace('-',' ',$slug))
-    //));
+
+        
     }
+
 }
+
+
+
 ?>
