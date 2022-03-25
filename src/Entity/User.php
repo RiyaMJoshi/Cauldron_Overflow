dae18ae4bec2c1b969d45db31ab407e549331491
Riya Joshi <riyajoshi312@gmail.com>
2022-03-25 18:20:28 +0530

    WITHOUT DROPDOWN: Authenticators Upto Custom User Methods & the User in a Service: Ch-25

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -152,4 +152,19 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getAvatarUri(int $size = 32): string
+    {
+        // https://ui-avatars.com/api/?name={{ app.user.firstName|url_encode }}&size=32&background=random
+        return 'https://ui-avatars.com/api/?' . http_build_query([
+            'name' => $this->getDisplayName(),
+            'size' => $size,
+            'background' => 'random',
+        ]);
+    }
+
+    public function getDisplayName(): string
+    {
+        return $this->getFirstName() ?: $this->getEmail();
+    }
 }
